2699aa6cb10a5ec28b81c53ac3f22ade713a83bc
User <daanishraj@gmail.com>
2024-10-17 12:46:37 +0200

    implement basic login page

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import UserAvatar from "./components/avatar";
+import Login from "./pages/login";
 import PageNotFound from "./pages/not-found";
 import Occassions from "./pages/occassions";
 import Profile from "./pages/profile";
@@ -9,7 +10,8 @@ const App = () => {
       <UserAvatar />
       <Router>
         <Routes>
-          <Route path="/" element={<Occassions />} />
+          {/* <Route path="/" element={<Occassions />} /> */}
+          <Route path="/" element={<Login />} />
           <Route path="/occasions" element={<Occassions />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="*" element={<PageNotFound />} />
